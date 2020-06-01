8860d4083d5b16fee4c1d7e8cd62c1642c68812d
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-06-01 17:42:44 +0600

    Needs to be added to the June 1 folder

---
+++ 9A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define REP(i, a, b) for (i = a; i < b; i++)
+#define REP_ITER(it, v) for (it = v.begin(); it != v.end(); it++)
+
+#define FASTIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(0)
+
+#define TEST(test) \
+    int test;      \
+    cin >> test;   \
+    while (test--)
+
+#define cll(a, b) cin >> a >> b
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define vi vector<int>
+#define vi_iterator vector<int>::iterator
+
+#define si set<int>
+#define si_iterator set<int>::iterator
+
+int main()
+{
+    FASTIO;
+    int y,k;
+    cin >> y >> k;
+    int ans = 7-max(y,k);
+    cout << ans/__gcd(ans,6) << "/" << 6/__gcd(ans,6) << endl;
+}
