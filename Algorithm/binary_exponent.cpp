6b7f144a0581b0f7298ebe10b3c243cf53d7b1a6
nafiz <naafizrahman1@gmail.com>
2020-06-17 18:45:06 +0600

    Two very very easy implementation problems solved

---
+++ Algorithm/binary_exponent.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define REP(i, a, b) for (i = a; i < b; i++)
+#define REP_ITER(it, v) for (it = v.begin(); it != v.end(); it++)
+
+#define FASTIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(0)
+
+#define TEST(test) \
+    int test;      \
+    cin >> test;   \
+    while (test--)
+
+#define cll(a, b) cin >> a >> b
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define vi vector<int>
+#define vi_iterator vector<int>::iterator
+
+#define si set<int>
+#define si_iterator set<int>::iterator
+
+ll power(ll a, ll b)
+{
+    ll result = 1;
+    while (b)
+    {
+        if (b % 2)
+            result *= a;
+        a *= a;
+        b /= 2;
+    }
+    return result;
+}
+
+int main()
+{
+    FASTIO;
+    cout << power(2, 0) << endl;
+}
