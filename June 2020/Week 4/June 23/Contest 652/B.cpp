a6b37e924657d94870a53845b469001f5a3a83b1
nafiz <naafizrahman1@gmail.com>
2020-06-23 22:06:49 +0600

    two solved ,C attempted

---
+++ June 2020/Week 4/June 23/Contest 652/B.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define REP(i, a, b) for (i = a; i < b; i++)
+#define REP_ITER(it, v) for (it = v.begin(); it != v.end(); it++)
+
+#define FASTIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(0)
+
+#define TEST(test) \
+    int test;      \
+    cin >> test;   \
+    while (test--)
+
+#define cll(a, b) cin >> a >> b
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define vi vector<int>
+#define vi_iterator vector<int>::iterator
+
+#define si set<int>
+#define si_iterator set<int>::iterator
+
+double pi = 2 * acos(0.0);
+int i, j;
+int main()
+{
+    FASTIO;
+    TEST(test)
+    {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll one = -1, zero = -1, yollow = 0;
+        REP(i, 0, s.size())
+        {
+            if (s[i] == '1' && one == -1)
+            {
+                one = i;
+            }
+            if (one != -1 && s[i] == '0')
+            {
+                yollow = 1;
+                zero = i;
+            }
+        }
+        if (!yollow)
+        {
+            cout << s << endl;
+        }
+        else
+        {
+            REP(i, 0, s.size())
+            {
+                if (i == one)
+                {
+                    cout << 0;
+                }
+                if (i >= one && i <= zero)
+                {
+                }
+                else
+                {
+                    cout << s[i];
+                }
+            }
+            cout << endl;
+        }
+    }
+}
---
+++ June 2020/Week 4/June 23/Contest 652/tempCodeRunnerFile.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define REP(i, a, b) for (i = a; i < b; i++)
+#define REP_ITER(it, v) for (it = v.begin(); it != v.end(); it++)
+
+#define FASTIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(0)
+
+#define TEST(test) \
+    int test;      \
+    cin >> test;   \
+    while (test--)
+
+#define cll(a, b) cin >> a >> b
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define vi vector<int>
+#define vi_iterator vector<int>::iterator
+
+#define si set<int>
+#define si_iterator set<int>::iterator
+
+double pi = 2 * acos(0.0);
+int i, j;
+int main()
+{
+    FASTIO;
+    TEST(test)
+    {
+        ll n, k, sum = 0;
+        cin >> n >> k;
+        ll arr[n], w[k];
+        map<ll, vector<ll>> mp;
+        map<ll, vector<ll>>::iterator it;
+        REP(i, 0, n)
+        {
+            cin >> arr[i];
+        }
+        REP(i, 0, k)
+        {
+            cin >> w[i];
+            sum += w[i];
+        }
+        sort(arr, arr + n, greater<int>());
+        sort(w, w + k);
+        ll x = 0;
+        ll ans = 0;
+        ll a = 0;
+        i = 0;
+        while (sum)
+        {
+            if (w[i % k])
+            {
+                mp[i % k].push_back(arr[x]);
+                x = (x + 1) % n;
+                sum--;
+                w[i % k]--;
+                i++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for (it = mp.begin(); it != mp.end(); it++)
+        {
+            ans += (*max_element(it->second.begin(), it->second.end()) + *min_element(it->second.begin(), it->second.end()));
+        }
+        
+        cout << ans << endl;
+    }
+}
