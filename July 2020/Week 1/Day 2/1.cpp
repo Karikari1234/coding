09ab62377451d5b87175055560ad247a014ac73a
nafiz <naafizrahman1@gmail.com>
2020-07-02 14:05:05 +0600

    4 problems solved

---
+++ July 2020/Week 1/Day 2/1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define REP(i, a, b) for (i = a; i < b; i++)
+#define REP_ITER(it, v) for (it = v.begin(); it != v.end(); it++)
+
+#define FASTIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(0)
+
+#define TEST(test) \
+    int test;      \
+    cin >> test;   \
+    while (test--)
+
+#define cll(a, b) cin >> a >> b
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define vi vector<int>
+#define vi_iterator vector<int>::iterator
+
+#define si set<int>
+#define si_iterator set<int>::iterator
+
+double pi = 2 * acos(0.0);
+int i, j;
+int main()
+{
+    FASTIO;
+    TEST(test)
+    {
+        ll l, r, d, t, flag = 0;
+        cin >> l >> r >> d >> t;
+        ll x, y, xl, yl, xr, yr;
+        cin >> x >> y >> xl >> yl >> xr >> yr;
+        if (x >= xl && x <= xr && y >= yl && y <= yr && (xr > xl || l + r == 0) && (yr > xl || t + d == 0))
+        {
+            YES;
+        }
+        else
+        {
+            NO;
+        }
+    }
+}
